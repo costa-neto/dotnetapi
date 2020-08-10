84d7d43b55a9322a9a140c7c572b12acb25cfc33
Neto Costa <neto_costa_1809@hotmail.com>
2020-08-10 03:24:29 -0300

    FirstCommit

---
+++ ProfileLogic/Profile.cs
@@ -0,0 +1,15 @@
+
+
+
+namespace dotnet_api.ProfileLogic{
+
+    public class Profile{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Bio { get; set; }
+        public string Age { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace dotnet_api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:3000")
+                .UseStartup<Startup>();
+                
+    }
+}
